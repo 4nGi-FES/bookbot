1ede8d0b445215547fbc24805c431a3d202fa8e7
4nGi-FES <official4nGi@gmail.com>
2025-10-25 19:10:25 -0400

    my attempt at a sorted dict list for stats.py and main.py, which passed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_word_count
 from stats import get_letter_frequency
+from stats import sort_on
+from stats import sorted_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -11,7 +13,15 @@ def main():
     text = get_book_text(file_path)
     num_words = get_word_count(text)
     num_char = get_letter_frequency(text)
-    return print(f"Found {num_words} total words"), print(num_char)
+    sort_char = sorted_characters(num_char)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in sort_char:
+        print(f"{item["char"]}:",item["num"])
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -12,4 +12,22 @@ def get_letter_frequency(text):
         else:
             char_dict[character] = 1
     return char_dict
-        
+
+def sort_on(items):
+    return items["num"]
+
+def sorted_characters(num_char):
+    char_dict = num_char
+    char_list = list(char_dict)
+    alpha_list = []
+    for char in char_list:
+        if char.isalpha() == True:
+            alpha_list.append(char)
+    data_list = []
+    for char in alpha_list:
+        val = char_dict[char]
+        data_pair = {"char": char, "num": val}
+        data_list.append(data_pair)
+    data_list.sort(reverse=True, key=sort_on)
+    return data_list
+
