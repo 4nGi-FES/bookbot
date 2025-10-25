1bbb6e26e1ae69fa6c383efbe693e2ae4fcd4097
4nGi-FES <official4nGi@gmail.com>
2025-10-25 19:29:05 -0400

    use sys.argv to make main.py flexible to analyze other books

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
@@ -5,12 +7,19 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = get_book_path()
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
+
+def get_book_path():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        return sys.argv[1]
 
 def get_book_text(path):
     with open(path) as f:
