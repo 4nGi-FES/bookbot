65636fe87e4f52c18a017e011be64ebcdb066af0
4nGi-FES <official4nGi@gmail.com>
2025-10-25 14:17:43 -0400

    main.py update - count Frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def get_word_count(text):
+    word_count = len(text.split())
+    return word_count
+
+def main():
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)
+    num_words = get_word_count(text)
+    return print(f"Found {num_words} total words")
+
+main()
